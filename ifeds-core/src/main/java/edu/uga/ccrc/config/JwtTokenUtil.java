739d3f41191ea9335729e2359716112cd5d8ff2a
akshaykokane <akshaykokane@192.168.1.11>
2019-11-10 18:18:53 -0500

    - updated size of token to 25

--- ifeds-core/src/main/java/edu/uga/ccrc/config/JwtTokenUtil.java
+++ ifeds-core/src/main/java/edu/uga/ccrc/config/JwtTokenUtil.java
@@ -15,8 +15,11 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtTokenUtil implements Serializable {
+	
 	private static final long serialVersionUID = -2550185165626007488L;
+	
 	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+	
 	@Value("${jwt.secret}")
 	private String secret;
 	
@@ -61,7 +64,7 @@ public class JwtTokenUtil implements Serializable {
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
 		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
-				.signWith(SignatureAlgorithm.HS512, secret).compact();
+				.signWith(SignatureAlgorithm.HS256, secret).compact();
 	}
 	//validate token
 	public Boolean validateToken(String token, UserDetails userDetails) {
