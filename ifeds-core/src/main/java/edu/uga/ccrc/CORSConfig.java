d53b7f73b3b09ad1b93528b37bb5b0c05d65017b
akshaykokane <akshaykokane09@gmail.com>
2020-02-19 20:44:50 -0500

    - configured global CORS policy

---
+++ ifeds-core/src/main/java/edu/uga/ccrc/CORSConfig.java
@@ -0,0 +1,16 @@
+package edu.uga.ccrc;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORSConfig implements WebMvcConfigurer  {
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				 registry.addMapping("/**");
+			}
+		};
+	}
+
+}
