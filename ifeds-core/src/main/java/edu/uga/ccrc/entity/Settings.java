116224862696c523ad201e88a19934df54b09e1c
akshaykokane <akshaykokane09@gmail.com>
2020-03-23 12:14:13 -0400

    - setting webservice

---
+++ ifeds-core/src/main/java/edu/uga/ccrc/entity/Settings.java
@@ -0,0 +1,68 @@
+package edu.uga.ccrc.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="settings", schema="core"
+	)
+public class Settings {
+	
+	public Settings(String key, String value) {
+		// TODO Auto-generated constructor stub
+		
+		this.key = key;
+		this.value = value;
+	}
+
+	protected Settings() {}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="sr_no", nullable=false)
+	private Long sr_no;
+	
+	
+	@Column(name="key", length=100, nullable=false)
+	private String key;
+	
+	
+	@Column(name="value", length=100, nullable=false)
+	private String value;
+
+
+	public Long getSr_no() {
+		return sr_no;
+	}
+
+
+	public void setSr_no(Long sr_no) {
+		this.sr_no = sr_no;
+	}
+
+
+	public String getKey() {
+		return key;
+	}
+
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+
+	public String getValue() {
+		return value;
+	}
+
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
