fa992a13bca2b1e8ce84257dc7d3d72e642af7d0
akshaykokane <akshaykokane09@gmail.com>
2020-04-10 14:03:50 -0400

    - added Dashboard bean

---
+++ ifeds-core/src/main/java/edu/uga/ccrc/view/bean/DashboardBean.java
@@ -0,0 +1,22 @@
+package edu.uga.ccrc.view.bean;
+
+public class DashboardBean {
+	
+	private int num_of_samples;
+	private int num_of_dataset;
+	
+	public int getNum_of_dataset() {
+		return num_of_dataset;
+	}
+	public void setNum_of_dataset(int num_of_dataset) {
+		this.num_of_dataset = num_of_dataset;
+	}
+	public int getNum_of_samples() {
+		return num_of_samples;
+	}
+	public void setNum_of_samples(int num_of_samples) {
+		this.num_of_samples = num_of_samples;
+	}
+	
+
+}
